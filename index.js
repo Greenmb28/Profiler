fc5ccb12c444add7d51180caf642f6069f1ca7c8
Greenmb28 <greenmb@purdue.edu>
2025-03-27 11:15:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+ const profiler = () =>
+ {
+
+ $("#name").text("Annie Student");
+ $("#major").text(" CS");
+ $("#year").text(" 2025") ;
+
+ $('h4').text("USA");
+
+ }
+ 
+$(document).ready(profiler)
